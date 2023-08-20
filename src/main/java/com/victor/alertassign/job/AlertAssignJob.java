2fe20c71b95b39a446bcd02d17568b91b12600cf
victorEsantos <victor.eduardo.sntos@gmail.com>
2023-08-20 20:04:54 -0300

    JobAdded

---
+++ src/main/java/com/victor/alertassign/job/AlertAssignJob.java
@@ -0,0 +1,16 @@
+package com.victor.alertassign.job;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class AlertAssignJob implements Job {
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        System.out.println("Job executado às " + new Date());
+    }
+}
---
+++ src/main/java/com/victor/alertassign/job/config/SchedulingConfig.java
@@ -0,0 +1,24 @@
+package com.victor.alertassign.job.config;
+
+import com.victor.alertassign.job.AlertAssignJob;
+import org.quartz.JobExecutionException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+    @Bean
+    public AlertAssignJob myScheduledJob() {
+        return new AlertAssignJob();
+    }
+
+//    @Scheduled(cron = "0 0 7 ? * MON")
+//    @Scheduled(cron = "* * * * * ?")
+    @Scheduled(cron = "0 * * ? * *")
+    public void runMyJob() throws JobExecutionException {
+        myScheduledJob().execute(null);
+    }
+}
