b399944658d11fbf5ee7c12a48b32418ac52a0b7
victorEsantos <victor.eduardo.sntos@gmail.com>
2023-11-18 22:21:10 -0300

    teste

--- src/main/java/com/victor/alertassign/AlertAssignApplication.java
+++ src/main/java/com/victor/alertassign/AlertAssignApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlertAssignApplication {
 
     public static void main(String[] args) {
+        System.out.println(">>>>>>>>Starting AlertAssignApplication...");
         SpringApplication.run(AlertAssignApplication.class, args);
     }
 
