06f7c41efdb2b7158a8ca4cdf167aa9e43eb3c33
victorEsantos <victor.eduardo.sntos@gmail.com>
2023-11-18 22:38:18 -0300

    rodar pipe

--- src/main/java/com/victor/alertassign/AlertAssignApplication.java
+++ src/main/java/com/victor/alertassign/AlertAssignApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlertAssignApplication {
 
     public static void main(String[] args) {
-        System.out.println(">>>>>>>>Starting AlertAssignApplication...");
         SpringApplication.run(AlertAssignApplication.class, args);
     }
 
